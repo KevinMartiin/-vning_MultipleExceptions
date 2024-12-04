b8f1a776713391a74600a8a452dea26199dd100d
kevinmartiin <nextgenallu@hotmail.com>
2024-12-04 10:37:30 +0100

    Add project files.

---
+++ Övning_MultipleExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Övning_MultipleExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Testfall
+            HandleMultipleExceptions("2", 1); 
+            HandleMultipleExceptions("abc", 1);
+            HandleMultipleExceptions("2", 5);
+        }
+
+        static void HandleMultipleExceptions(string inputString, int index)
+        {
+            
+            int[] numbers = { 1, 2, 3 };
+
+            try
+            {
+                
+                int parsedNumber = int.Parse(inputString);
+
+                Console.WriteLine(numbers[index]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format.");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index out of range.");
+            }
+        }
+    }
+}
